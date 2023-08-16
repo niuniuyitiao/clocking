301e1731753447abc5e6ac0c66d6f3937653fc6b
niupengfei <niupengfei@ovopark.com>
2023-08-16 14:54:15 +0800

    token 设置

--- util/request/requestInterceptors.js
+++ util/request/requestInterceptors.js
@@ -4,7 +4,7 @@ module.exports = (vm) => {
     uni.$u.http.interceptors.request.use((config) => { // 可使用async await 做异步操作
         // 初始化请求拦截器时，会执行此方法，此时data为undefined，赋予默认{}
         config.data = config.data || {};
-		config.header.Authorization = getToken() || '';
+		config.header.Authorization = 'Bearer '+(getToken() || '');
 		console.log(777888, config)
         // 可以在此通过vm引用vuex中的变量，具体值在vm.$store.state中
         // console.log(vm.$store.state);
